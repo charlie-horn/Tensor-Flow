056a1c2665f8e24bb4d170021d758175f628698a
charlie-horn <charlie.horn@queensu.ca>
2019-04-04 08:03:47 -0400

    Added session class, node traversal, and basic example for z = Ax + b

--- main.py
+++ main.py
@@ -11,6 +11,30 @@ class Operation():
     def compute(self):
         pass
 
+class add(Operation):
+    def __init__(self,x,y):
+        super().__init__([x,y])
+
+    def compute(self, x_var, y_var):
+        self.inputs = [x_var, y_var]
+        return x_var + y_var
+
+class multiply(Operation):
+    def __init__(self,x,y):
+        super().__init__([x,y])
+
+    def compute(self,x_var, y_var):
+        self.inputs = [x_var,y_var]
+        return x_var*y_var
+
+class matmul(Operation):
+    def __init__(self,x,y):
+        super().__init__([x,y])
+
+    def compute(self,x_var, y_var):
+        self.inputs = [x_var,y_var]
+        return x_var.dot(y_var)
+
 class Placeholder():
     def __init__(self):
         self.output_nodes = []
@@ -30,7 +54,43 @@ class Graph():
         self.placeholders = []
         self.variables = []
 
-    def set_as_default():
+    def set_as_default(self):
         global _default_graph
         _default_graph = self
 
+def traverse_postorder(operation):
+    nodes_postorder = []
+    def recurse(node):
+        if isinstance(node, Operation):
+            for input_node in node.input_nodes:
+                recurse(input_node)
+        nodes_postorder.append(node)
+    recurse(operation)
+    return nodes_postorder
+
+class Session():
+    def run(self, operation, feed_dict={}):
+        nodes_postorder = traverse_postorder(operation)
+        for node in nodes_postorder:
+            if type(node) == Placeholder:
+                node.output = feed_dict[node]
+            elif type(node) == Variable:
+                node.output = node.value
+            else:
+                node.inputs = [input_node.output for input_node in node.input_nodes]
+                node.output = node.compute(*node.inputs)
+            if type(node.output) == list:
+                node.output = np.array(node.output)
+        return operation.output
+g = Graph()
+g.set_as_default()
+
+A = Variable(10)
+b = Variable(1)
+x = Placeholder()
+y = multiply(A,x)
+z = add(y,b)
+
+sess = Session()
+result = sess.run(operation=z, feed_dict={x:10})
+print(result)
